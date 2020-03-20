39b1b63617611032a7868e6d25b4777da2382562
Simon Lerch <simon.lerch@innoactive.de>
2020-03-20 12:05:17 +0100

    Updated the template to fit into the assembly, namespace definitions

---
+++ Editor/TemplateSceneSetup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Innoactive.CreatorEditor.BasicTemplate
+{
+    public class TemplateSceneSetup : OnSceneSetup
+    {
+        public override void Setup()
+        {
+            if (GameObject.Find("[CAMERA_CONFIGURATION]") == null)
+            {
+                GameObject cameraConfig = (GameObject)GameObject.Instantiate(Resources.Load("CustomCamera/Prefabs/[CAMERA_CONFIGURATION]"));
+                cameraConfig.name = "[CAMERA_CONFIGURATION]";
+            }
+        }
+    }
+}
+
