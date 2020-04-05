33c57b9a22e2e6eb4a9760357cecf179d85e4d20
Gustavo Quiroz <gusinuhe@gmail.com>
2020-04-05 15:35:45 +0200

    chg: PostProcesingPackageValidator was replaced for PostProcessingPackageEnabler which is being used by the DependencyManager.

---
+++ Editor/PackageDependencies/PostProcessingPackageEnabler.cs
@@ -0,0 +1,14 @@
+﻿namespace Innoactive.CreatorEditor.PackageManager.BasicTemplate
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PostProcessingPackageEnabler : Dependency
+    {
+        /// <inheritdoc/>
+        public override string Package { get; } = "com.unity.postprocessing";
+
+        /// <inheritdoc/>
+        public override int Priority { get; } = 10;
+    }
+}
