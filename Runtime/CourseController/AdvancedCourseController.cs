be0e62c6da256fd1aa2b2d9fcdaf8671d27ab1a3
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-10-13 10:19:55 +0200

    fix: mambo jumbo logic expression
    Co-authored-by: Simon L <simon.lerch@innoactive.de>

--- Runtime/CourseController/AdvancedCourseController.cs
+++ Runtime/CourseController/AdvancedCourseController.cs
@@ -541,7 +541,7 @@ namespace Innoactive.Creator.BaseTemplate
                 
                 // If there is only one option, the dropdown is currently disabled.
                 chapterPicker.enabled = dropdownOptions.Count > 1;
-                if (dropdownOptions.Count > 1 == false)
+                if (dropdownOptions.Count <= 1)
                 {
                     
                     ColorBlock colorBlock = chapterPicker.colors;
